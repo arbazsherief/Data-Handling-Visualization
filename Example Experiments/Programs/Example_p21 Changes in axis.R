89697044fd54a36b9a759619c5a8dbe81ed8e591
arbazsherief <113408671+arbazsherief@users.noreply.github.com>
2024-03-02 13:27:23 +0530

    Delete Example Experiments directory

--- Example Experiments/Programs/Example_p21 Changes in axis.R
+++
@@ -1,8 +0,0 @@
-library(ggplot2)  
-# Giving a name to the chart file.  
-png(file = "scatterplot2.png")  
-# Plotting the chart using ggplot() and geom_point() functions.  
-#The aes() function inside the geom_point() function controls the color of the group.  
-ggplot(mtcars, aes(x = log(mpg), y = log(drat))) +geom_point(aes(color=factor(gear)))  
-# Saving the file.  
-dev.off()  
