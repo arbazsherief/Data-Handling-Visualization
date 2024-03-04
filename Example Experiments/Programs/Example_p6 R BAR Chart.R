1839a41bc00dc3f87b3378c966816617c39daecb
arbazsherief <113408671+arbazsherief@users.noreply.github.com>
2024-03-04 09:58:26 +0530

    Delete Example Experiments directory

--- Example Experiments/Programs/Example_p6 R BAR Chart.R
+++
@@ -1,8 +0,0 @@
-# Creating the data for Bar chart  
-H<- c(12,35,54,3,41)  
-# Giving the chart file a name  
-png(file = "bar_chart.png")  
-# Plotting the bar chart   
-barplot(H)  
-# Saving the file  
-dev.off()  
--- Example Experiments/Programs/Example_p9 R Boxplot.R
+++
@@ -1,6 +0,0 @@
-png(file = "boxplot.png")  
-# Plotting the chart.  
-boxplot(mpg ~ cyl, data = mtcars, xlab = "Quantity of Cylinders",  
-        ylab = "Miles Per Gallon", main = "R Boxplot Example")  
-# Save the file.  
-dev.off()  
